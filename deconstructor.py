e0f0f21b352505c03af2c3645b64faf5ca66410d
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-09-30 20:21:55 +0530

    Update deconstructor.py

--- deconstructor.py
+++ deconstructor.py
@@ -4,17 +4,18 @@ DISCORD_EPOCH = 1420070400
 
 def convert_snowflake(snowflake):
   milliseconds = int(snowflake >> 22)/1000
-  return datetime.fromtimestamp(milliseconds + DISCORD_EPOCH).isoformat()
+  return datetime.fromtimestamp(milliseconds + DISCORD_EPOCH).isoformat() + 'Z'
 
 def validate_snowflake():
   while True:
     try:
-      snowflake = int(input("Snowflake: ")
-      if snowflake > 9999999999999999999:
-        print("Error! Snowflakes have lower digits"); continue 
+      snowflake = int(input("Snowflake: "))
       if snowflake < 4194304:
-        print("Error! Snowflakes are large numbers"); continue 
+        print("Error! Snowflakes are large numbers")
+        continue 
       print(convert_snowflake(snowflake))
+    except OverflowError:
+      print("Error! Snowflakes are lower numbers")
     except ValueError:
       print("Snowflakes only contain numbers")
 
