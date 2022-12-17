bb4c97542b227a601db7b70c58941e212f772c4a
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-12-17 20:55:11 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import sys
+
 from snowflake_converter import validate_snowflake
+
 def main():
   # Parse command-line arguments or prompt user for snowflake
   if len(sys.argv) == 2:
     snowflake = sys.argv[1]
   else:
     snowflake = input("Enter a snowflake: ")
+
   # Convert snowflake and print result
   date_string = validate_snowflake(snowflake)
-  print(f"Snowflake {snowflake} corresponds to date {date_string}")
+  if isinstance(date_string, str):
+    print(date_string)
+  else:
+    print(f"Snowflake {snowflake} corresponds to date {date_string}")
 
 if __name__ == '__main__':
   main()
