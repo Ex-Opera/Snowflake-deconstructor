80485c840ec75985d34642f1e4a22963294c1eb7
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-12-17 20:04:01 +0530

    Added docstrings
    Improved code readability

--- deconstructor.py
+++ deconstructor.py
@@ -1,19 +1,35 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
+# Discord epoch timestamp in seconds
 DISCORD_EPOCH = 1420070400
 
-def convert_snowflake(snowflake):
-  milliseconds = int(snowflake >> 22)/1000
-  return datetime.fromtimestamp(milliseconds + DISCORD_EPOCH).isoformat() + 'Z'
+def convert_snowflake(snowflake: int) -> str:
+  """Convert snowflake to ISO format date string.
+  Args:
+      snowflake (int): Snowflake to convert.
+  Returns:
+      str: ISO format date string in UTC.
+  """
+  # Calculate number of milliseconds since Discord epoch
+  milliseconds = int(snowflake >> 22)
+  # Add milliseconds to Discord epoch and return ISO format date string in UTC
+  return (datetime.utcfromtimestamp(DISCORD_EPOCH) + timedelta(milliseconds=milliseconds)).isoformat() + 'Z'
 
-def validate_snowflake():
+def validate_snowflake() -> str:
+  """Validate snowflake and convert to ISO format date string.
+  Returns:
+      str: ISO format date string in UTC.
+  """
   while True:
     try:
+      # Snowflake to validate
       snowflake = int(input("Snowflake: "))
+      # Check if snowflake is a valid 64-bit integer
       if snowflake < 4194304:
         print("Error! Snowflakes are large numbers")
       else:
         print(convert_snowflake(snowflake))
+        break
     except OverflowError:
       print("Error! Snowflakes are lower numbers")
     except ValueError:
